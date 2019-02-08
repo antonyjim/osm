5b0661590aa75933a1d2dcd205913f34077ca3f5
Anthony Jund <antonyjund@gmail.com>
2019-02-07 19:07:35 -0500

    Ignore .env

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,7 +7,7 @@ import { readFileSync } from 'fs';
 
 import routes from './app'
 const dotenv = require('dotenv')
-const envConfig = dotenv.parse(readFileSync(__dirname + '/../env_var.env'))
+const envConfig = dotenv.parse(readFileSync(__dirname + '/../dot.env'))
 
 for (let k in envConfig) {
     process.env[k] = envConfig[k]
