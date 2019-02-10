da23df8f5a371a065910af3229289422fadf88fb
Anthony Jund <antonyjund@gmail.com>
2019-02-10 10:51:36 -0500

    Implement tests and cluster for production

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,7 +4,8 @@
 */
 
 // Node Modules
-
+import * as cluster from 'cluster'
+import { cpus } from 'os'
 
 // NPM Modules
 import * as express from 'express'
@@ -14,15 +15,36 @@ import { router } from './routes/index'
 import { Log } from './lib/log';
 
 export default function routes() {
-    // Constants and global variables
-    const app = express()
-    const port = 8020
-    // Routes
-    app.disable('x-powered-by')
-    app.use('/', router)
+    if (process.env.NODE_ENV === 'production') {
+        const cores: number = cpus().length
+        if (cluster.isMaster) {
+            console.log('Started cluster, master process as ' + process.pid)
+            let i: number = parseInt(process.env.CLUSTER_COUNT) || cores
+            while(i >= 0) {
+                cluster.fork()
+                i--
+            }
 
-    app.listen(port, function() {
-        new Log(`Listening at port ${port}`).info()
-        console.log(`Listening on port ${port}`);
-    })
+            cluster.on('exit', (worker, code, signal) => {
+                console.log(`Worker process ${worker.process.pid} crashed`)
+            })
+        } else {
+            listen()
+        }
+    } else {
+        listen()
+    }
+
+    function listen() {
+        // Constants and global variables
+        const app = express()
+        const port = parseInt(process.env.SERVER_PORT) || 8020
+        // Routes
+        app.disable('x-powered-by')
+        app.use('/', router)
+
+        app.listen(port, function() {
+            new Log(`Listening at port ${port} on process ${process.pid}`).info()
+        })
+    }
 }
---
+++ test/routes/index.js
@@ -0,0 +1,14 @@
+//@ts-check
+/**
+ * Test routing
+ */
+var chai = require('chai')
+var expect = chai.expect
+var indexPage = require('../../dist/routes/ui/index')
+
+describe('getLoginPage', function() {
+    it('should return the login page with no token', function() {
+        let req = {}
+        
+    })
+})
