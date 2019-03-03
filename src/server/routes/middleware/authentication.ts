38cf2ecaff7a04edb69e8cab8ea09f71e9ee1620
Anthony Jund <antonyjund@gmail.com>
2019-03-03 09:02:56 -0500

    Add tslint and prettier to project. (Yes I know it's a little late to be doing this)

--- src/server/routes/middleware/authentication.ts
+++ src/server/routes/middleware/authentication.ts
@@ -1,11 +1,10 @@
 /**
  * routes.middleware.authentication.ts
- * 
+ *
  * Handle validation of endpoints and token issuance
-*/
+ */
 
 // Node Modules
-
 
 // NPM Modules
 import { Request, Response, NextFunction } from 'express'
@@ -14,195 +13,246 @@ import { sign, verify } from 'jsonwebtoken'
 // Local Modules
 import { validateEndpoint } from './../../lib/navigation/navigation'
 import { StatusMessage } from './../../types/server'
-import { UserTypes } from '../../types/users';
-import { jwtSecret } from '../../lib/connection';
+import { UserTypes } from '../../types/users'
+import { jwtSecret } from '../../lib/connection'
 
 // Constants and global variables
 
+/**
+ * Validate tokens present in the cookie of the request, really only applicable to the main login/app page
+ */
 export function tokenValidation() {
-    return function tokenValidation(req: Request, res: Response, next: NextFunction) {
-        let anonToken: UserTypes.AuthToken = {
-            iA: false,
-            u: null,
-            r: null,
-            c: null
-        }
-        let tokenCookie = req.query.token || req.cookies.token
-        if (tokenCookie) {
-            tokenCookie = tokenCookie.split('Bearer ')[1] || tokenCookie
-        }
-        req.auth = {}
-        function handleOnAuthError(error: Error) {
-            req.auth = {
-                iA: false,
-                iZ: false,
-                u: null,
-                c: null
-            }
-            sign(anonToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                if (err) console.error(err)
-                req.auth = {
-                    iA: false,
-                    iZ: false,
-                    u: null,
-                    c: null
-                }
-                res.cookie('token', token)
-                return next()
-            })
-        }
-        if (!tokenCookie) {
-            sign(anonToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                if (err) handleOnAuthError(err)
-                req.auth = {
-                    iA: false,
-                    iZ: false,
-                    u: null,
-                    c: null
-                }
-                res.cookie('token', token)
-                return next()
-            })
-        } else {
-            verify(tokenCookie, jwtSecret, function(err: Error, decoded: UserTypes.AuthToken) {
-                if (err) handleOnAuthError(err)
-                if (decoded) {
-                    let authToken: UserTypes.AuthToken = {
-                        iA: true,
-                        u: decoded.u,
-                        r: decoded.r,
-                        c: decoded.c
-                    }
-                    sign(authToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                        if (err) handleOnAuthError(err)
-                        req.auth = {
-                            iA: true,
-                            iZ: false,
-                            u: decoded.u,
-                            r: decoded.r,
-                            c: decoded.c
-                        }
-                        res.cookie('token', token)
-                        return next()
-                    })
-                } else {
-                    handleOnAuthError(new Error('Expired token'))
-                }
-            })
-        }
+  return (req: Request, res: Response, next: NextFunction) => {
+    const anonToken: UserTypes.AuthToken = {
+      iA: false,
+      u: null,
+      r: null,
+      c: null
     }
+    let tokenCookie = req.query.token || req.cookies.token
+    if (tokenCookie) {
+      tokenCookie = tokenCookie.split('Bearer ')[1] || tokenCookie
+    }
+    req.auth = {}
+    function handleOnAuthError(error: Error) {
+      req.auth = {
+        iA: false,
+        iZ: false,
+        u: null,
+        c: null
+      }
+      sign(
+        anonToken,
+        jwtSecret,
+        { expiresIn: '1h' },
+        (err: Error, token: string) => {
+          if (err) console.error(err)
+          req.auth = {
+            iA: false,
+            iZ: false,
+            u: null,
+            c: null
+          }
+          res.cookie('token', token)
+          return next()
+        }
+      )
+    }
+    if (!tokenCookie) {
+      sign(
+        anonToken,
+        jwtSecret,
+        { expiresIn: '1h' },
+        (err: Error, token: string) => {
+          if (err) handleOnAuthError(err)
+          req.auth = {
+            iA: false,
+            iZ: false,
+            u: null,
+            c: null
+          }
+          res.cookie('token', token)
+          return next()
+        }
+      )
+    } else {
+      verify(
+        tokenCookie,
+        jwtSecret,
+        (err: Error, decoded: UserTypes.AuthToken) => {
+          if (err) handleOnAuthError(err)
+          if (decoded) {
+            const authToken: UserTypes.AuthToken = {
+              iA: true,
+              u: decoded.u,
+              r: decoded.r,
+              c: decoded.c
+            }
+            sign(
+              authToken,
+              jwtSecret,
+              { expiresIn: '1h' },
+              (signErr: Error, token: string) => {
+                if (signErr) handleOnAuthError(signErr)
+                req.auth = {
+                  iA: true,
+                  iZ: false,
+                  u: decoded.u,
+                  r: decoded.r,
+                  c: decoded.c
+                }
+                res.cookie('token', token)
+                return next()
+              }
+            )
+          } else {
+            handleOnAuthError(new Error('Expired token'))
+          }
+        }
+      )
+    }
+  }
 }
 
+/**
+ * Validate the token located in the token query parameter on API requests only
+ */
 export function apiTokenValidation() {
-    return function tokenValidation(req: Request, res: Response, next: NextFunction) {
-        let anonToken: UserTypes.AuthToken = {
-            iA: false,
-            u: null,
-            r: null,
-            c: null
-        }
-        let tokenCookie = req.query.token
-        if (tokenCookie) {
-            tokenCookie = tokenCookie.split('Bearer ')[1] || tokenCookie
-        }
-        req.auth = {}
-        function handleOnAuthError(error: Error) {
-            req.auth = {
-                iA: false,
-                iZ: false,
-                u: null,
-                c: null
-            }
-            sign(anonToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                if (err) res.status(500).end()
-                req.auth = {
-                    iA: false,
-                    iZ: false,
-                    u: null,
-                    c: null
-                }
-                req.auth.t = token
-                res.status(401).json({
-                    error: true,
-                    message: 'User unauthenticated or token expired'
-                })
-            })
-        }
-        if (!tokenCookie) {
-            sign(anonToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                if (err) handleOnAuthError(err)
-                req.auth = {
-                    iA: false,
-                    iZ: false,
-                    u: null,
-                    c: null
-                }
-                req.auth.t = token
-                res.status(401).json({
-                    error: true,
-                    message: 'User unauthenticated or token expired'
-                })
-            })
-        } else {
-            verify(tokenCookie, jwtSecret, function(err: Error, decoded: UserTypes.AuthToken) {
-                if (err) handleOnAuthError(err)
-                if (decoded) {
-                    let authToken: UserTypes.AuthToken = {
-                        iA: true,
-                        u: decoded.u,
-                        r: decoded.r,
-                        c: decoded.c
-                    }
-                    sign(authToken, jwtSecret, {expiresIn: '1h'}, function(err: Error, token: string) {
-                        if (err) handleOnAuthError(err)
-                        req.auth = {
-                            iA: true,
-                            iZ: false,
-                            u: decoded.u,
-                            r: decoded.r,
-                            c: decoded.c
-                        }
-                        req.auth.t = token
-                        next()
-                    })
-                } else {
-                    handleOnAuthError(new Error('Expired token'))
-                }
-            })
-        }
+  return (req: Request, res: Response, next: NextFunction) => {
+    const anonToken: UserTypes.AuthToken = {
+      iA: false,
+      u: null,
+      r: null,
+      c: null
     }
+    let tokenCookie = req.query.token
+    if (tokenCookie) {
+      tokenCookie = tokenCookie.split('Bearer ')[1] || tokenCookie
+    }
+    req.auth = {}
+    function handleOnAuthError(error: Error) {
+      req.auth = {
+        iA: false,
+        iZ: false,
+        u: null,
+        c: null
+      }
+      sign(
+        anonToken,
+        jwtSecret,
+        { expiresIn: '1h' },
+        (err: Error, token: string) => {
+          if (err) res.status(500).end()
+          req.auth = {
+            iA: false,
+            iZ: false,
+            u: null,
+            c: null
+          }
+          req.auth.t = token
+          res.status(401).json({
+            error: true,
+            message: 'User unauthenticated or token expired'
+          })
+        }
+      )
+    }
+    if (!tokenCookie) {
+      sign(
+        anonToken,
+        jwtSecret,
+        { expiresIn: '1h' },
+        (err: Error, token: string) => {
+          if (err) handleOnAuthError(err)
+          req.auth = {
+            iA: false,
+            iZ: false,
+            u: null,
+            c: null
+          }
+          req.auth.t = token
+          res.status(401).json({
+            error: true,
+            message: 'User unauthenticated or token expired'
+          })
+        }
+      )
+    } else {
+      verify(
+        tokenCookie,
+        jwtSecret,
+        (err: Error, decoded: UserTypes.AuthToken) => {
+          if (err) handleOnAuthError(err)
+          if (decoded) {
+            const authToken: UserTypes.AuthToken = {
+              iA: true,
+              u: decoded.u,
+              r: decoded.r,
+              c: decoded.c
+            }
+            sign(
+              authToken,
+              jwtSecret,
+              { expiresIn: '1h' },
+              (signErr: Error, token: string) => {
+                if (signErr) handleOnAuthError(signErr)
+                req.auth = {
+                  iA: true,
+                  iZ: false,
+                  u: decoded.u,
+                  r: decoded.r,
+                  c: decoded.c
+                }
+                req.auth.t = token
+                next()
+              }
+            )
+          } else {
+            handleOnAuthError(new Error('Expired token'))
+          }
+        }
+      )
+    }
+  }
 }
 
+/**
+ * Validate the endpoint that is attepmting to be accessed by comparing
+ * the role in the token to the roles on the sys_navigation table.
+ */
 export function endpointAuthentication() {
-    return function endpointAuthentication(req: Request, res: Response, next: NextFunction) {
-        if (!req.auth || !req.auth.iA || !req.auth.u) {
-            req.auth.iZ = false
-            console.log(JSON.stringify(req.auth))
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.auth || !req.auth.iA || !req.auth.u) {
+      req.auth.iZ = false
+      console.log(JSON.stringify(req.auth))
+      return res.status(401).json({
+        error: true,
+        message: 'User unauthenticated or token expired'
+      })
+    } else {
+      validateEndpoint(req.method, req.originalUrl, req.auth.r)
+        .then(
+          (onUserAuthorized: StatusMessage) => {
+            req.auth.iZ = onUserAuthorized.details.authorized
+            next()
+          },
+          (onUserUnAuthorized: StatusMessage) => {
+            req.auth.iZ = onUserUnAuthorized.details.authorized
+            console.log('User is not authorized')
             return res.status(401).json({
-                error: true,
-                message: 'User unauthenticated or token expired'
+              error: true,
+              message: 'User unauthorized with current privileges'
             })
-        } else {
-            validateEndpoint(req.method, req.originalUrl, req.auth.r)
-            .then((onUserAuthorized: StatusMessage) => {
-                req.auth.iZ = onUserAuthorized.details.authorized
-                next()
-            }, (onUserUnAuthorized: StatusMessage) => {
-                req.auth.iZ = onUserUnAuthorized.details.authorized
-                console.log('User is not authorized')
-                return res.status(401).json({
-                    error: true,
-                    message: 'User unauthorized with current privileges'
-                })
-            })
-            .catch((error: StatusMessage) => {
-                console.error(error)
-                req.auth.iZ = false
-                return res.status(401).json({
-                    error: true,
-                    message: 'User authorization failed'
-                })
-            })
-        }
+          }
+        )
+        .catch((error: StatusMessage) => {
+          console.error(error)
+          req.auth.iZ = false
+          return res.status(401).json({
+            error: true,
+            message: 'User authorization failed'
+          })
+        })
     }
+  }
 }
--- src/server/routes/ui/index.ts
+++ src/server/routes/ui/index.ts
@@ -1,21 +1,21 @@
 /**
  * routes.ui.index.ts
  * Provide a route for all UI routes
-*/
+ */
 
 // Node Modules
-import { createReadStream } from 'fs';
-import { resolve } from 'path';
+import { createReadStream } from 'fs'
+import { resolve } from 'path'
 
 // NPM Modules
 import { Router, Request, Response } from 'express'
 
 // Local Modules
 import { tokenValidation } from './../middleware/authentication'
-import bodyParser = require('body-parser');
+import bodyParser = require('body-parser')
 import loginRoutes from './login'
-import verifyRoutes from './verification';
-import { getServerStatus } from '../../lib/utils';
+import verifyRoutes from './verification'
+import { getServerStatus } from '../../lib/utils'
 
 // Constants and global variables
 const uiRoutes = Router()
@@ -25,60 +25,67 @@ uiRoutes.use(tokenValidation())
 uiRoutes.use('/login', loginRoutes)
 uiRoutes.use('/verify', verifyRoutes)
 
-uiRoutes.get('/logout', function(req: Request, res: Response) {
-    res.cookie('token', null)
-    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-    let fileStream = createReadStream(resolve(__dirname, '../../../static/login.html'))
-    fileStream.on('data', function(data) {
-        res.write(data)
-    })
-    fileStream.on('end', function() {
-        res.end()
-        return
-    })
+uiRoutes.get('/logout', (req: Request, res: Response) => {
+  res.cookie('token', null)
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
+  let fileStream = createReadStream(
+    resolve(__dirname, '../../../static/login.html')
+  )
+  fileStream.on('data', function(data) {
+    res.write(data)
+  })
+  fileStream.on('end', function() {
+    res.end()
+    return
+  })
 })
 
 uiRoutes.get('/stats', function(req: Request, res: Response) {
-    getServerStatus()
-    .then(stats => {
-        res.status(200).json(stats)
-    })
+  getServerStatus().then((stats) => {
+    res.status(200).json(stats)
+  })
 })
 
 uiRoutes.get('/wetty', function(req: Request, res: Response) {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-        let fileStream = createReadStream(resolve(__dirname, '../../../static/wettyError.html'))
-        fileStream.on('data', function(data) {
-            res.write(data)
-        })
-        fileStream.on('end', function() {
-            res.end()
-            return
-        })
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
+  let fileStream = createReadStream(
+    resolve(__dirname, '../../../static/wettyError.html')
+  )
+  fileStream.on('data', function(data) {
+    res.write(data)
+  })
+  fileStream.on('end', function() {
+    res.end()
+    return
+  })
 })
 
 uiRoutes.get('*', function(req: Request, res: Response) {
-    if (req.auth.iA && req.auth.u) {
-        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-        let fileStream = createReadStream(resolve(__dirname, '../../../static/index.html'))
-        fileStream.on('data', function(data) {
-            res.write(data)
-        })
-        fileStream.on('end', function() {
-            res.end()
-            return
-        })
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-        let fileStream = createReadStream(resolve(__dirname, '../../../static/login.html'))
-        fileStream.on('data', function(data) {
-            res.write(data)
-        })
-        fileStream.on('end', function() {
-            res.end()
-            return
-        })
-    }
+  if (req.auth.iA && req.auth.u) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
+    let fileStream = createReadStream(
+      resolve(__dirname, '../../../static/index.html')
+    )
+    fileStream.on('data', function(data) {
+      res.write(data)
+    })
+    fileStream.on('end', function() {
+      res.end()
+      return
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
+    let fileStream = createReadStream(
+      resolve(__dirname, '../../../static/login.html')
+    )
+    fileStream.on('data', function(data) {
+      res.write(data)
+    })
+    fileStream.on('end', function() {
+      res.end()
+      return
+    })
+  }
 })
 
 export { uiRoutes }
