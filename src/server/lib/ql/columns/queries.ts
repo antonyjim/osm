1758be607bb028cafe98cb2324a83cd977bdd6e0
Anthony Jund <antonyjund@gmail.com>
2019-03-03 16:59:01 -0500

    Tweak linter

--- src/server/lib/ql/columns/queries.ts
+++ src/server/lib/ql/columns/queries.ts
@@ -1,8 +1,10 @@
 import Column from './columns'
 
 const columnQueries = {
-    sys_db_dictionary: ((queryFields, id, context) => new Column(context, queryFields).getById(id)),
-    sys_db_dictionary_list: ((queryFields, fields, context, pagination) => new Column(context, queryFields).getFields(fields, pagination))
+  sys_db_dictionary: (queryFields, id, context) =>
+    new Column(context, queryFields).getById(id),
+  sys_db_dictionary_list: (queryFields, fields, context, pagination) =>
+    new Column(context, queryFields).getFields(fields, pagination)
 }
 
 export default columnQueries
