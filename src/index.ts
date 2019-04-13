5fc49f0f726adb77f60664ea302fb3825c858b40
Anthony Jund <antonyjund@gmail.com>
2019-04-12 22:00:06 -0400

    Fix bug with reading .env

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ readFile(
     lines.map((line: string) => {
       const key: string = line.split('=')[0]
       const value: string = line.split('=')[1]
-      if (value[0] === "'") {
+      if (value && value[0] === "'") {
         console.log(
           '[STARTUP] Setting environment variable %s to value %s',
           key,
